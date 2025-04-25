ba71c3e5d49f5011b67f46f4a7caae49b9755cc8
PCicciari <cicciari.pietro@gmail.com>
2025-04-25 13:39:02 -0700

    updated server to custom segmentation

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
 import pixellib
-from pixellib.instance import instance_segmentation
+from pixellib.instance import custom_segmentation
 
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests from React
@@ -14,8 +14,9 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 # Load PixelLib model
-segment_image = instance_segmentation()
-segment_image.load_model("mask_rcnn_coco.h5")  # or your own model
+segment_image = custom_segmentation()
+segment_image.inferConfig(num_classes=1, class_names = ["BG", "tumor"])
+segment_image.load_model("(0.59)mask_rcnn_model.025-1.435254.h5")  # or your own model
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
@@ -25,7 +26,7 @@ def upload_file():
     output_path = os.path.join(OUTPUT_FOLDER, f"segmented_{filename}")
     file.save(input_path)
 
-    segment_image.segmentImage(image_path=input_path, output_image_name=output_path)
+    segment_image.segmentImage(input_path, output_image_name=output_path)
 
     return send_file(output_path, mimetype="image/png")
 
